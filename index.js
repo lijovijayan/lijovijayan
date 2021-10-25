9ce2e3bfffcd0486203c85e3a25b1bbe50c1428e
lijovijayan <lijovijayan00@gmail.com>
2021-10-25 23:39:36 +0530

    updated deployment config

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const ghpages = require("gh-pages");
+var fs = require("fs");
+
 const appPath = "../portfolio/build/";
-ghpages.publish(appPath, function (err) {
+const domain = "lijovijayan.dev";
+
+fs.writeFile(`${appPath}CNAME`, domain, function (err) {
   console.log(err);
+});
+ghpages.publish(appPath, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`application is published at https://${domain}`);
+  }
 });
