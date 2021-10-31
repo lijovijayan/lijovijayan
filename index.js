04e2a27a35e1b1dc62c00947e8887466d0a935e1
lijovijayan <lijovijayan00@gmail.com>
2021-10-31 18:44:32 +0530

    added 404 file

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ var fs = require("fs");
 const appPath = "../portfolio/build/";
 const domain = "lijovijayan.dev";
 
+// setting domain name for github pages to detect
 fs.writeFile(`${appPath}CNAME`, domain, function (err) {
   console.log(err);
 });
+
+// setting up 404 page
+fs.writeFile(
+  `${appPath}404.html`,
+  `<meta http-equiv="refresh" content="0; url=https://${domain}" />`,
+  function (err) {
+    console.log(err);
+  }
+);
+
 ghpages.publish(appPath, function (err) {
   if (err) {
     console.log(err);
